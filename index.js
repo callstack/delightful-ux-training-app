b5c069c29c4f64d0ef03480332319763bc97ebf2
NoemiRozpara <nrozpara@gmail.com>
2019-02-21 12:51:47 +0100

    chore: rename main screen, add folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import MainScreen from './MainScreen';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => MainScreen);
