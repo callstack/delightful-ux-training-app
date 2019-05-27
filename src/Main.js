8e72067c04351c689c5638ffa3601656b2b8965d
Trancever <dawidu@onet.pl>
2019-05-27 16:00:54 +0200

    feat: init expo app and setup eslint + prettier

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class MainComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Main Component</Text>
+      </View>
+    );
+  }
+}
