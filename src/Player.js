02c32d0ca4751f72592da905924e06915523edba
Michał Pierzchała <thymikee@gmail.com>
2019-06-07 15:15:01 +0200

    feat: add draggable progress bar

--- src/Player.js
+++ src/Player.js
@@ -4,17 +4,32 @@ import {
   Text,
   StyleSheet,
   Dimensions,
-  SafeAreaView,
   TouchableOpacity,
 } from 'react-native';
-import Animated, { Easing } from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
-//import { withTheme } from '@callstack/react-theme-provider';
 
 import { runTiming } from './utils';
 import { PLAYER_HEIGHT } from './constants';
 
-const { Clock, Value, stopClock, cond, set, block, eq, neq } = Animated;
+const {
+  Clock,
+  Value,
+  stopClock,
+  cond,
+  set,
+  block,
+  eq,
+  neq,
+  event,
+  add,
+  sub,
+} = Animated;
+
+const backgroundColor = '#21262c';
+const primaryColor = '#FFF';
+const contrastColor = '#F8F32B';
 
 class Player extends React.PureComponent {
   static defaultProps = {
@@ -29,6 +44,10 @@ class Player extends React.PureComponent {
   playerPosition = new Value(100);
   progressBarPosition = new Value(0);
   maxProgressBarPosition = (Dimensions.get('window').width || 27) - 27; // paddings + indicator size / 2
+
+  dragX = new Value(0);
+  state = new Value(-1);
+  prevDragX = new Value(0);
 
   componentDidUpdate(prevProps) {
     if (this.props.currentSong !== prevProps.currentSong) {
@@ -55,14 +74,14 @@ class Player extends React.PureComponent {
   };
 
   render() {
-    const backgroundColor = '#21262c',
-      primaryColor = '#FFF',
-      contrastColor = '#F8F32B';
+    this._onGestureEvent = event([
+      { nativeEvent: { translationX: this.dragX, state: this.state } },
+    ]);
 
     return (
       <Animated.View
         style={[
-          style.container,
+          styles.container,
           {
             backgroundColor,
             transform: [{ translateY: this.playerPosition }],
@@ -94,31 +113,52 @@ class Player extends React.PureComponent {
                 eq(this.progressBarPosition, this.maxProgressBarPosition),
                 set(this.progressBarPosition, 0)
               ),
+              cond(
+                eq(this.state, State.ACTIVE),
+                [
+                  set(
+                    this.progressBarPosition,
+                    add(
+                      this.progressBarPosition,
+                      sub(this.dragX, this.prevDragX)
+                    )
+                  ),
+                  set(this.prevDragX, this.dragX),
+                  set(this.playingState, 0),
+                ],
+                set(this.prevDragX, 0)
+              ),
             ])
           }
         </Animated.Code>
         <Text style={{ color: primaryColor }}>{this.props.currentSong}</Text>
-        <View style={style.controls}>
-          <TouchableOpacity style={style.control} onPress={this.pause}>
+        <View style={styles.controls}>
+          <TouchableOpacity style={styles.control} onPress={this.pause}>
             <Ionicons name="md-pause" size={32} color={contrastColor} />
           </TouchableOpacity>
-          <TouchableOpacity style={style.control} onPress={this.play}>
+          <TouchableOpacity style={styles.control} onPress={this.play}>
             <Ionicons name="md-play" size={32} color={contrastColor} />
           </TouchableOpacity>
-          <TouchableOpacity style={style.control} onPress={this.hidePlayer}>
+          <TouchableOpacity style={styles.control} onPress={this.hidePlayer}>
             <Ionicons name="md-square" size={32} color={contrastColor} />
           </TouchableOpacity>
         </View>
-        <View style={[style.progressBar, { backgroundColor: primaryColor }]}>
-          <Animated.View
-            style={[
-              style.progressIndicator,
-              {
-                transform: [{ translateX: this.progressBarPosition }],
-                backgroundColor: contrastColor,
-              },
-            ]}
-          />
+        <View style={[styles.progressBar, { backgroundColor: primaryColor }]}>
+          <PanGestureHandler
+            maxPointers={1}
+            onGestureEvent={this._onGestureEvent}
+            onHandlerStateChange={this._onGestureEvent}
+          >
+            <Animated.View
+              style={[
+                styles.progressIndicator,
+                {
+                  transform: [{ translateX: this.progressBarPosition }],
+                  backgroundColor: contrastColor,
+                },
+              ]}
+            />
+          </PanGestureHandler>
         </View>
       </Animated.View>
     );
@@ -127,11 +167,10 @@ class Player extends React.PureComponent {
 
 export default Player;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     height: PLAYER_HEIGHT,
     justifyContent: 'center',
-    alignItems: 'center',
     alignItems: 'center',
     position: 'absolute',
     bottom: 0,
@@ -156,8 +195,8 @@ const style = StyleSheet.create({
   },
   progressIndicator: {
     top: -4,
-    height: 10,
-    width: 10,
+    height: 20,
+    width: 20,
     left: 0,
     position: 'relative',
     borderRadius: 5,
