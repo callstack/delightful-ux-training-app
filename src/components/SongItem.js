eb02f161c7af213f76fad6e969eff92581f5bb35
NoemiRozpara <nrozpara@gmail.com>
2019-09-03 15:08:23 +0200

    feat: song drag

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Text, StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
 
 import FavouriteIcon from './FavouriteIcon';
 import SmallSongImage from './SmallSongImage';
 import { ROW_HEIGHT } from '../utils/constants';
 
+const { event, Value } = Animated;
+
 class SongItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.translateX = new Value(0);
+
+    this.onGestureEvent = event([
+      {
+        nativeEvent: {
+          translationX: this.translateX,
+        },
+      },
+    ]);
+  }
+
   handleHideEnd = () => {
     this.props.onSongRemove(this.props.item.track.id);
   };
@@ -22,23 +40,38 @@ class SongItem extends React.Component {
     return (
       <TouchableWithoutFeedback onPress={this.handlePress}>
         <View style={styles.container}>
-          <View style={styles.song}>
-            <View style={styles.innerContainer}>
-              <SmallSongImage uri={track.album.images[0].url} />
-              <View style={styles.title}>
-                <Text style={styles.titleText} numberOfLines={1}>
-                  {track.name}
-                </Text>
-                <Text style={styles.subtitleText} numberOfLines={1}>
-                  {track.album.name}
-                </Text>
+          <PanGestureHandler
+            onGestureEvent={this.onGestureEvent}
+            onHandlerStateChange={this.onGestureEvent}
+            maxPointers={1}
+            activeOffsetX={10}
+          >
+            <Animated.View
+              style={[
+                styles.song,
+                {
+                  transform: [{ translateX: this.translateX }],
+                  height: ROW_HEIGHT,
+                },
+              ]}
+            >
+              <View style={styles.innerContainer}>
+                <SmallSongImage uri={track.album.images[0].url} />
+                <View style={styles.title}>
+                  <Text style={styles.titleText} numberOfLines={1}>
+                    {track.name}
+                  </Text>
+                  <Text style={styles.subtitleText} numberOfLines={1}>
+                    {track.album.name}
+                  </Text>
+                </View>
+                <FavouriteIcon
+                  onToggle={() => this.props.onSongFavouriteToggle(track.id)}
+                  checked={this.props.item.isFavourite}
+                />
               </View>
-              <FavouriteIcon
-                onToggle={() => this.props.onSongFavouriteToggle(track.id)}
-                checked={this.props.item.isFavourite}
-              />
-            </View>
-          </View>
+            </Animated.View>
+          </PanGestureHandler>
         </View>
       </TouchableWithoutFeedback>
     );
@@ -50,7 +83,6 @@ export default SongItem;
 const styles = StyleSheet.create({
   container: {
     overflow: 'hidden',
-    height: ROW_HEIGHT,
   },
   song: {
     flex: 1,
